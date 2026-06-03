7832d874149aa9879f2701e9a58ce727de0eea78
0mkar22 <obhogate48@gmail.com>
2026-06-03 18:34:41 +0530

    updated environment variables for dashboard

--- tron-dashboard/next.config.mjs
+++ tron-dashboard/next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     env: {
         SUPABASE_URL: process.env.SUPABASE_URL,
         SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+        BACKEND_URL: process.env.BACKEND_URL,
     }
 };
 
