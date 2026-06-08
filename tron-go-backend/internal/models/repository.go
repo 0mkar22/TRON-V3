a2684b8bb4be701476fd95018b9830dec10d3fa8
0mkar22 <obhogate48@gmail.com>
2026-06-08 16:23:16 +0530

    fixing the duplicate key error for repositories

--- tron-go-backend/internal/models/repository.go
+++ tron-go-backend/internal/models/repository.go
@@ -8,16 +8,14 @@ import (
 
 type Repository struct {
 	ID                  string         `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
-	OrgID               string         `gorm:"type:uuid" json:"org_id"`
-	RepoName            string         `gorm:"type:text;not null;unique" json:"repo_name"`
-	PMProvider          string         `gorm:"type:text;not null;column:pm_provider" json:"pm_provider"`
+	OrgID               string         `gorm:"type:uuid;uniqueIndex:repo_provider_unique" json:"org_id"`
+	RepoName            string         `gorm:"type:text;not null;uniqueIndex:repo_provider_unique" json:"repo_name"`
+	PMProvider          string         `gorm:"type:text;not null;column:pm_provider;uniqueIndex:repo_provider_unique" json:"pm_provider"`
 	PMProjectID         string         `gorm:"type:text;column:pm_project_id" json:"pm_project_id"`
 	Mapping             datatypes.JSON `gorm:"type:jsonb;default:'{}'::jsonb" json:"mapping"`
 	CommunicationConfig datatypes.JSON `gorm:"type:jsonb;default:'{}'::jsonb" json:"communication_config"`
 	CreatedAt           time.Time      `gorm:"default:now()" json:"created_at"`
-
-	// Foreign Key relationship
-	Organization Organization `gorm:"foreignKey:OrgID"`
+	Organization        Organization   `gorm:"foreignKey:OrgID"`
 }
 
 func (Repository) TableName() string {
